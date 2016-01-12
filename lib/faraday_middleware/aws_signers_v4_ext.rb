06902c4993e7c9ecd1084236bf1b16436e81b7d2
Mayank Kohaley <mayank.kohaley@gmail.com>
2016-01-12 15:36:52 +0530

    Ruby 1.9.3 compatibility

--- lib/faraday_middleware/aws_signers_v4_ext.rb
+++ lib/faraday_middleware/aws_signers_v4_ext.rb
@@ -1,13 +1,9 @@
 require 'aws-sdk-core/signers/v4'
 
-module AwsSignersV4Ext
-  def signed_headers(request)
-    super.downcase
-  end
-end
-
 class Aws::Signers::V4
-  unless Aws::Signers::V4 < AwsSignersV4Ext
-    prepend AwsSignersV4Ext
-  end
+ alias signed_headers_orig signed_headers
+ 
+ def signed_headers(request)
+   signed_headers_orig(request).downcase
+ end
 end
